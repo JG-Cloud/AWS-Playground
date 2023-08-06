cbdeae242af79910567af4a8fce9e50a553ac903
Jatin Gandhi <jatin.kumar.gandhi@gmail.com>
2023-08-07 00:09:32 +0100

    Updating working script

--- stop-start-AWS-compute/README.md
+++ stop-start-AWS-compute/README.md
@@ -1,5 +1,3 @@
-# WIP
-
 # Stop or start instances based on tag: dev
 
 To run the script, sys argv - '**stop**' or '**start**' on execution of script from cmdline should be specified 
@@ -8,6 +6,8 @@ To run the script, sys argv - '**stop**' or '**start**' on execution of script f
 
 
 The script will stop or start instances based on tag: dev
+
+        Note: *tag: dev* will need to updated inside the script, however will look to make this a feature in the future as a sys.argv
 
 Script should typically be run as a cronjob on a schedule, i.e. stop at 7pm every evening mon-fri, and start again at 7am
 
--- stop-start-AWS-compute/stop-start-AWS-compute.py
+++ stop-start-AWS-compute/stop-start-AWS-compute.py
@@ -1,9 +1,16 @@
 #!/bin/python3
 
-# Stop or start instances based on tag: dev. 
-# Script should typically be run as a cronjob on a schedule, i.e. stop at 7pm every evening mon-fri, and start again at 7am. 
-# For all of sat/sun, the instances should remain off.
-# 
+"""
+Stop or start AWS ec2 instances based on tag: dev. 
+Script should typically be run as a cronjob on a schedule, i.e. stop at 7pm every evening mon-fri, and start again at 7am. 
+For all of sat/sun, the instances should remain off.
+
+To execute script run:
+$./stop-start-AWS-compute.py stop
+or
+$./stop-start-AWS-compute.py start
+"""
+
 
 import boto3
 import sys
@@ -11,18 +18,32 @@ from pprint import pprint
 
 client = boto3.client('ec2')
 
+def main():
+    stop_start_input = sys_argv()
+    get_ec2_data(stop_start_input)
+
 
 # add logic for sys argv - 'stop' or 'start' on execution of script from cmdline
 def sys_argv():
-    if sys.argv[1] and sys.argv[1] == 'start' or sys.argv[1] == 'stop':
-        return sys.argv[1]
-    else:
-        stop_start_prompt = input(f"Enter 'stop' or 'start' to take this action on your ec2s\n")
-        return stop_start_prompt
+    if len(sys.argv) >= 2:
+        if sys.argv[1] == 'start' or sys.argv[1] == 'stop':
+            return sys.argv[1]
         
+        else:
+            print(f"Exiting script due to incorrect entry...\n'stop' or 'start' are the only valid entries...")
+            sys.exit(1)
+
+    if len(sys.argv) < 2:
+        stop_start_prompt = input(f"Enter 'stop' or 'start':\n> ").lower()
+        if stop_start_prompt == 'stop' or stop_start_prompt == 'start':
+            return stop_start_prompt
+        else:
+            print(f"Exiting script due to incorrect entry...\n'stop' or 'start' are the only valid entries...")
+            sys.exit(1)            
+
 
 # get instanceIDs/Names where tag eq dev
-def get_ec2_data():
+def get_ec2_data(stop_start_input, Token=None):
     response = client.describe_instances(
         Filters=[
             {
@@ -46,20 +67,32 @@ def get_ec2_data():
                     InstanceName = tag['Value']
 
         #print(f"{InstanceId} : {InstanceName}")
-        stop_start_ec2s(InstanceId, InstanceName)
-    
-
-# add instanceIDs and Names to an array
+        stop_start_ec2s(InstanceId, InstanceName, stop_start_input)
 
 
-# for each instanceID in array, stop or start instances based on sys argv
-def stop_start_ec2s(InstanceId, InstanceName):
-    if sys_argv() == 'stop':
-        response = client.stop_instances(
+# for each instanceID, stop or start instances based on sys argv
+def stop_start_ec2s(InstanceId, InstanceName, stop_start_input):
+    if stop_start_input == 'stop':
+        stop_response = client.stop_instances(
             InstanceIds=[
             InstanceId,
             ]
         )
+        pprint(stop_response['StoppingInstances'])
+        
+    if stop_start_input == 'start':
+        start_response = client.start_instances(
+            InstanceIds=[
+            InstanceId,
+            ]
+        )
+        pprint(start_response)
+
+    # print list of instanceIDs/Names that were stopped or started
+    print(f"Instance={InstanceId}:{InstanceName}\n")
 
 
-# print list of instanceIDs/Name that were stopped or started
+main()
+
+
+
