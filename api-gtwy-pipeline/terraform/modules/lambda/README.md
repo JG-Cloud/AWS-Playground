e817d53079c6e24ae5519ff7ec6e63b1ca0322ec
Jatin Gandhi <jatin.kumar.gandhi@gmail.com>
2023-08-22 00:45:07 +0100

    adding lambda config

---
+++ api-gtwy-pipeline/terraform/modules/lambda/README.md
@@ -0,0 +1,3 @@
+# To add custom modules to lambda, follow steps;
+
+https://repost.aws/knowledge-center/lambda-import-module-error-python
--- api-gtwy-pipeline/terraform/modules/lambda/test_lambda.py
+++ api-gtwy-pipeline/terraform/modules/lambda/test_lambda.py
@@ -1,23 +1,29 @@
+from botocore.vendored import requests
 import requests
 import freecurrencyapi
 import os
 from pprint import pprint
+import json
+import urllib3
 
-# Currency API source: https://exchangerate.host
+def lambda_handler(event, context):
+    print(event)
+    return currency_rate()
 
-url = 'https://api.exchangerate.host/latest/?base=GBP'
-response = requests.get(url)
-data = response.json()
-
-allrates = data['rates']
-
-for currency, rate in allrates.items():
-    if 'AED' in currency:
-        AEDrate = rate
+def currency_rate():
+    url = 'https://api.exchangerate.host/latest/?base=GBP'
+    response = requests.get(url)
+    data = response.json()
     
-    if 'USD' in currency:
-        USDrate = rate
-          
-print(f"Base currency is GBP:")
-print(f"AED : {AEDrate}")
-print(f"USD : {USDrate}")
+    allrates = data['rates']
+    
+    for currency, rate in allrates.items():
+        if 'AED' in currency:
+            AEDrate = rate
+        
+        if 'USD' in currency:
+            USDrate = rate
+              
+    print(f"Base currency is GBP:")
+    print(f"AED : {AEDrate}")
+    print(f"USD : {USDrate}")
